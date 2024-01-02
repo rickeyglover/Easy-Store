95c75839074178d364e055c05f0df67383f883a9
Steven <stevccru@icloud.com>
2024-01-02 12:23:21 -0500

    modified some changes

--- capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -48,12 +48,12 @@ public class CategoriesController
 
     // the url to return all products in category 1 would look like this
     // https://localhost:8080/categories/1/products
-    @GetMapping("categories/{categoryId}/products")
-    public List<Product> getProductsById(@PathVariable int categoryId)
-    {
+    @GetMapping("/{categoryId}/products")
+    public List<Product> getProductsById(@PathVariable int categoryId) {
         // get a list of product by categoryId
         return productDao.listByCategoryId(categoryId);
     }
+
 
     // add annotation to call this method for a POST action
     @PostMapping("/categories")
